0b7508fdedb88c89044f1b58149ddf57b845c1cd
Chris Fitzmorris <cfitz93@gmail.com>
2017-10-14 04:52:14 -0500

    Featured client/story and more edit ui

--- rails_server/app/models/client.rb
+++ rails_server/app/models/client.rb
@@ -1,6 +1,8 @@
 class Client < ApplicationRecord
   has_many :events
 
+  validates_presence_of :name
+
   def to_s
     name
   end
--- rails_server/db/schema.rb
+++ rails_server/db/schema.rb
@@ -10,16 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171014072806) do
+ActiveRecord::Schema.define(version: 20171014093508) do
 
-  create_table "clients", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "bio"
-    t.string "intro"
-    t.string "embed_html"
-  end
+# Could not dump table "clients" because of following StandardError
+#   Unknown type 'bool' for column 'featured'
 
   create_table "events", force: :cascade do |t|
     t.string "description"
