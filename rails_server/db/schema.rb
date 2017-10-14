8d8086da9f04ed77b1972f3c285f2df28afc9847
PuneethBikkumanla <pbikkumanla@gmail.com>
2017-10-14 12:37:12 -0400

    added code for backend

--- rails_server/db/schema.rb
+++ rails_server/db/schema.rb
@@ -35,6 +35,11 @@ ActiveRecord::Schema.define(version: 20171014095838) do
     t.index ["client_id"], name: "index_events_on_client_id"
   end
 
+  create_table "new_users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
